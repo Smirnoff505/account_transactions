856d28442765f51bd03a005d9060c6ac8384790d
Oleg Smirnov <oleg.smirnoff505@yandex.ru>
2023-08-15 00:23:45 +0500

    Добавление тестов test_last_five_transactions, date_formation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,25 @@
+import pytest
+
 from src import utils
 
-test_dict = {"id": 41428829,
-             "state": "EXECUTED",
-             "date": "2019-07-03T18:35:29.512364",
-             "operationAmount": {
-                 "amount": "8221.37",
-                 "currency": {
-                     "name": "USD",
-                     "code": "USD"
-                 }
-             },
-             "description": "Перевод организации",
-             "from": "MasterCard 7158300734726758",
-             "to": "Счет 35383033474447895560"
-             },
+transaction_test = {"id": 41428829,
+                    "state": "EXECUTED",
+                    "date": "2019-07-03T18:35:29.512364",
+                    "operationAmount": {
+                        "amount": "8221.37",
+                        "currency": {
+                            "name": "USD",
+                            "code": "USD"
+                        }
+                    },
+                    "description": "Перевод организации",
+                    "from": "MasterCard 7158300734726758",
+                    "to": "Счет 35383033474447895560"
+                    },
+
+
+def test_last_five_transactions():
+    pass
 
 
 def test_hide_number():
@@ -21,4 +27,8 @@ def test_hide_number():
 
 
 def test_amount():
-    assert utils.amount(test_dict) == '8221.37 USD'
+    assert utils.amount(transaction_test) == "8221.37 USD"
+
+
+def date_formation():
+    pass
